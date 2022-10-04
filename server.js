a42b85983e20cc6c9ed022dc133899925bdaf6d0
Samim Kohistani <samimkohistani@samims-air.myfiosgateway.com>
2022-10-04 01:21:40 -0400

    connection events

--- server.js
+++ server.js
@@ -23,4 +23,10 @@ mongoose.connect(MONGODB_URL, {
     useNewUrlParser: true,
   });
 
-  
+
+  // Connection Events
+mongoose.connection
+.on("open", () => console.log("You are connected to mongoose"))
+.on("close", () => console.log("You are disconnected from mongoose"))
+.on("error", (error) => console.log(error));
+
