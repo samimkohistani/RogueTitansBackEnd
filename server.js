0a6d3dab8a152276f4479ac11a7003edbd6182a7
Samim Kohistani <samimkohistani@samims-air.myfiosgateway.com>
2022-10-04 01:19:10 -0400

    Dependencies

---
+++ server.js
@@ -0,0 +1,15 @@
+// DEPENDENCIES
+// get .env variables
+require("dotenv").config();
+// pull PORT from .env, give default value of 4949
+// pull MONGODB_URL from .env
+const { PORT = 4949, MONGODB_URL } = process.env;
+// import express
+const express = require("express");
+// create application object
+const app = express();
+// import mongoose
+const mongoose = require("mongoose");
+// import middlware
+const cors = require("cors");
+const morgan = require("morgan");
